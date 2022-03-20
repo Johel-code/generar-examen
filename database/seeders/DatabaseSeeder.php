fcf989512acfb46b7895e00d7e09d9f79942d46f
Johel Saavedra <201910122@est.umss.edu>
2022-03-19 23:52:33 -0400

    Inicialización del proyecto

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Faculty;
+use App\Models\Career;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Faculty::factory(3)->create();
+        Career::factory(15)->create();
+    }
+}
