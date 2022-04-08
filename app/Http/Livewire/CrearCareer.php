5ba63d2a55fb58f8944d3fa5a2f76cdcf14560bc
Johel Saavedra <201910122@est.umss.edu>
2022-04-08 00:53:06 -0400

    crear career component vista and controller

--- app/Http/Livewire/CrearCareer.php
+++ app/Http/Livewire/CrearCareer.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Database\Eloquent\Model;
 use Livewire\Component;
 
 class CrearCareer extends Component
 {
+    public Model $model;
+
     public function render()
     {
         return view('livewire.crear-career');
